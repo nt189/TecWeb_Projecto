c1016d245a1e30e130876d87669e9634f9d50b13
AguilarAlexisGG <alexis.aguilarga@alumno.buap.mx>
2024-12-06 00:38:42 -0600

    Validar_Datos_Productos

--- backend/config.php
+++ backend/config.php
@@ -1,7 +1,7 @@
 <?php
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
-define('DB_PASS', 'zP*liGgdxEbBXjyk');
+define('DB_PASS', '1234');/*zP*liGgdxEbBXjyk*/ 
 define('DB_NAME', 'cafeteria');
 // ...existing code...
 ?>
